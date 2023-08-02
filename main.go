099e1d675732d0ecb03439e1f78271bc7502f203
Nick Olson <naolson.1819@gmail.com>
2023-08-02 16:32:10 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,12 @@
+// how do we execute this program?
+// go run main.go
+// the keyword 'package main' means this file will be compiled
+// into an executable program (binary) called main instead of a library
+package main
+
+// short for 'format'
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
